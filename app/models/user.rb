94db029e04e709946449276e22218c9ba56aae60
Asromain <mesprojetsinfo@gmail.com>
2014-05-21 16:06:44 -0700

    Ajout du module signin, pour un utilisateur admin enregistré (à la console)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
 	# sauvegarde l'email en minuscule
 	before_save { email.downcase! }
+	before_create :create_remember_token
 
 	# nom
 	validates :name,  presence: true, length: { maximum: 50 }
@@ -13,5 +14,19 @@ class User < ActiveRecord::Base
 	# mot de passe
 	has_secure_password
 	validates :password, length: { minimum: 6 }	
-	
+
+	def User.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def User.digest(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
+
+	private
+
+		def create_remember_token
+			self.remember_token = User.digest(User.new_remember_token)
+		end
+
 end
